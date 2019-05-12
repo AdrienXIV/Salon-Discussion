0dcade0355dfe87277bedd38d8b48b4136680df4
Adrien Maillard <adrien.mld@laposte.net>
2019-05-12 19:00:18 +0200

    auto identification + impossible de doublons dans la bdd + mdp chiffrés

--- serveur.js
+++ serveur.js
@@ -25,7 +25,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.get('/success', (req, res) => res.redirect('salle.html'));
-app.get('/error', (req, res) => res.send("Erreur identifiants"));
+app.get('/error', (req, res) => res.redirect("/"));
 
 passport.serializeUser(function (user, done) {
     //user => stocke un id comme clé dans le paramètre done
@@ -62,14 +62,30 @@ const identification = mongoose.model('identification', UserDetail, 'identificat
 
 //requête lors du formulaire d'inscription
 app.post("/inscription", (req, res) => {
+    let mdp = crypt(req.body.mdp);
     var myData = new identification({
         username: req.body.utilisateur,
-        password: req.body.mdp,
+        password: mdp,
         pseudo: req.body.pseudo
     });
-    myData.save(); //envoie des données à mongodb
-    res.redirect('salle.html')
+
+    var user = req.body;
+    identification.findOne({
+        username: user.utilisateur
+    }, (err, existingUser) => {
+        if (err) throw err;
+        else if (existingUser == null) {
+            console.log("n'existe pas")
+            myData.save();
+            res.redirect('/salle.html');
+        } else {
+            console.log("existe")
+            res.redirect('/');
+        }
+    })
 });
+
+
 
 /* PASSPORT LOCAL AUTHENTICATION */
 
@@ -84,7 +100,8 @@ passport.use(new LocalStrategy(
         identification.findOne({
             username: username
         }, function (err, user) { //user => myData = new Identification({})
-            console.log("parametre user : " + user);
+            //console.log("parametre user : " + user);
+
             if (err) {
                 return done(err);
             }
@@ -93,7 +110,7 @@ passport.use(new LocalStrategy(
                 return done(null, false);
             }
 
-            if (user.password != password) {
+            if (decrypt(user.password) != password) {
                 //user.password => password dans le schema de la base de données
                 return done(null, false);
             }
@@ -109,3 +126,28 @@ app.post('/connexion',
     function (req, res) {
         res.redirect('salle.html');
     });
+
+
+// CHIFFREMENT MOT DE PASSE
+const crypto = require('crypto');
+
+const algorithm = 'aes-192-cbc'; //algorithme de chiffrement
+const password = 'l5JmP+G0/1zB%;r8B8?2?2pcqGcL^3'; //clé de chiffrement
+
+const key = crypto.scryptSync(password, 'salt', 24);
+const iv = Buffer.alloc(16, 0); // Initialisation du vector.
+
+const cipher = crypto.createCipheriv(algorithm, key, iv); //chiffrer
+const decipher = crypto.createDecipheriv(algorithm, key, iv); //déchiffrer
+
+function decrypt(encrypted) {
+    let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+}
+
+function crypt(text) {
+    let crypted = cipher.update(text, 'utf8', 'hex');
+    crypted += cipher.final('hex');
+    return crypted;
+}
